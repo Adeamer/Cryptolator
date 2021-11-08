e31284ab3333a12b103da7694211b31994623ded
Elziekio <anthony.deamer@hotmail.com>
2021-11-08 18:04:18 +1000

    Amended sever to work with graphQL and Apollo

--- Server/server.js
+++ Server/server.js
@@ -4,8 +4,6 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 const path = require('path');
-const routes = require('./routes');
-
 
 
 const app = express();
@@ -29,8 +27,6 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
