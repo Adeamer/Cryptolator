7db4fdffecd66924d09151e17ba37c36bb173806
Elziekio <anthony.deamer@hotmail.com>
2021-11-10 18:41:01 +1000

    Adding quriers for wallet and currency

--- Server/schemas/resolvers.js
+++ Server/schemas/resolvers.js
@@ -5,67 +5,88 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
         user: async (parent, args, context) => {
-            if(context.user){
+            if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id }).select("-_v -password").populate("wallet")
                 return userData;
             }
 
             throw new AuthenticationError("Not Logged in");
-        }
+        },
+        wallet: async (parent, args, context) => {
+            if (context.user) {
+                return await User.findById(_id).populate('wallet');
+            }
 
+            throw new AuthenticationError("Not Logged in");
+        },
+        currency: async (parent, args, context) => {
+            if(context.wallet) {
+                return await Wallet.findById(_id).populate('currency');
+            }
+
+            throw new AuthenticationError("Not Logged in");
+        }
     },
 
-    Mutation: {
+    Mutations: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
             const token = signToken(user);
-            
-            return {token, user};
+
+            return { token, user };
         },
 
-        login: async (parent, {email, password}) => {
-            const user = await User.findOne({email});
-            
-            if(!user) {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
                 throw new AuthenticationError("Incorrect credentials");
             }
 
             const correctPw = await user.isCorrectPassword(password);
 
-            if(!correctPw) {
+            if (!correctPw) {
                 throw new AuthenticationError("Incorrect credentials");
             }
 
             const token = signToken(user);
-            return {token, user};
+            return { token, user };
         },
 
         addCurrency: async (parent, args, context) => {
-            if(conext.user) {
+            if (conext.user) {
 
                 const updatedUser = await User.findByIdAndUpdate(
                     { _id: context.user._id },
                     { $addToSet: { currency: args.input } },
                     { new: true }
                 );
-            return updatedUser;
+                return updatedUser;
             }
 
             throw new AuthenticationError('You need to be logged in!');
         },
 
         removeCurrency: async (parent, args, contaxt) => {
-            if(context.user) {
+            if (context.user) {
                 const updatedUser = await User.findOneAndupdate(
                     { _id: context.user._id },
                     { $pull: { currency: { currencyId: args.currencyId } } },
                     { new: true }
                 );
-            return updatedUser;
+                return updatedUser;
             }
 
             throw new AuthenticationError('You need to be logged in!');
         },
+
+        updateUser: async (parent, args, context) => {
+            if (context.user) {
+                return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
     }
 };
 
