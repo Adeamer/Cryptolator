d610ea3e987fb9afdab6992ab291ed67d7af9cfd
Elziekio <anthony.deamer@hotmail.com>
2021-11-10 18:40:34 +1000

    Added header sign up and login button

---
+++ Client/src/pages/home.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+const Home = () => {
+
+    return (
+        <div>
+            <header>Crypulator
+                <p>Brief explination of app</p>
+            </header>
+            <div>
+                <button>Sign Up</button>
+                <button>Login</button>
+                {/* Image of calculator and graph */}
+            </div>
+        </div>
+    )
+};
+
+export default Home;
