fa6ccf8c6c473710d6646a466f36093e0e0a8464
Elziekio <anthony.deamer@hotmail.com>
2021-11-11 21:16:50 +1000

    updated user model

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,21 +1,51 @@
 const { Schema, model } = require('mongoose');
 
+const currencySchema = require('./Currency');
+
 const userSchema = new Schema({
-    id: String,
-    name: String,
-    email: String,
-    wallet: [
-        {
-            name: String,
-            purchasedAmount: String,
-            soldAmount: String,
-            dateOfTransaction: Date,
-            yearlyIncome: String,
-            costOwning: String,
-            timeOfOwnership: Boolean
-        }
-    ]
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address']
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    currency: [currencySchema],
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
 });
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+// when we query a user, we'll also get another field called `currencyCount` with the number of currencies we have
+userSchema.virtual('currencyCount').get(function () {
+    return this.currency.length;
+  });
 
 const User = model('User', userSchema);
 
