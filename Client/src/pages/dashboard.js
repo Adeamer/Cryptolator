a4380367040ce19e0c07203dedcd30244b031e6b
Elziekio <anthony.deamer@hotmail.com>
2021-11-11 21:02:23 +1000

    Basic dashboard setup

---
+++ Client/src/pages/dashboard.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import Graph from '../components/Graph';
+
+const Dashboard = () => {
+
+    return (
+        <div>
+            <h1>Hello username</h1>
+            <button>Logout</button>
+            <div classname='walletContainer'>
+                <h2>Wallet</h2>
+                <p>Currencies</p>
+                <p>Amount</p>
+                <h3>Total</h3>
+            </div>
+            <div classname='graphContainer'>
+                <Graph />
+                <p>Total amount of tax</p>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard;
