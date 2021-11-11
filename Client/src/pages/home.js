f4940ae8dbf50c3df849fd90c6dffe8a249644c5
Elziekio <anthony.deamer@hotmail.com>
2021-11-11 20:22:50 +1000

    Adding basic home page

--- Client/src/pages/home.js
+++ Client/src/pages/home.js
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import SignupForm from '../components/SignupForm';
+import LoginForm from '../components/LoginForm';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { Routes, Route, Link } from 'react-router-dom';
+
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+});
 
 const Home = () => {
+
 
     return (
         <div>
             <header>Crypulator
                 <p>Brief explination of app</p>
             </header>
-            <div>
-                <button>Sign Up</button>
-                <button>Login</button>
+                <Link to="/signup">
+                    <button>Sign Up</button>
+                </Link>
+                <Link to="/login">
+                    <button>login</button>
+                </Link>
+                <Routes>
+                    <Route path='/signup' component={SignupForm}/>
+                    <Route path='/login' component={LoginForm}/>
+                </Routes>
                 {/* Image of calculator and graph */}
-            </div>
         </div>
     )
 };
