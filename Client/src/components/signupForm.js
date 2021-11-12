b8b3975d5dee4625b01c25b8760b0b4bf72c0c26
Elziekio <anthony.deamer@hotmail.com>
2021-11-12 21:27:42 +1000

    Updated form for signing up

--- Client/src/components/signupForm.js
+++ Client/src/components/signupForm.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { userMutation } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import 'antd/dist/antd.css';
 
-import { Form, Input, InputNumber, Button } from 'antd';
+import { Form, Input, Button } from 'antd';
 
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+import { Redirect } from 'react-router';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Dashboard from '../pages/Dashboard';
 
 const layout = {
   labelCol: {
@@ -26,106 +30,129 @@ const validateMessages = {
   },
 };
 
-const SignUpForm = () => {
-  const onFinish = (values) => {
-    console.log(values);
+
+
+const SignupForm = () => {
+
+    // set initial form state
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser] = useMutation(ADD_USER);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
   };
 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // use try/catch instead of promises to handle errors
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...userFormData }
+      });
+
+      Auth.login(data.addUser.token)
+
+    } catch (e) {
+      console.error(e);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+
+    console.log(userFormData);
+
+    <Redirect to='/dashboard'><Dashboard /></Redirect>
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  
 
   return (
     <div>
       <h1>Sign Up</h1>
-      <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-        <Form.Item
-          name={['user', 'name']}
-          label="Name"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={['user', 'email']}
-          label="Email"
-          rules={[
-            {
-              type: 'email',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={['user', 'age']}
-          label="Age"
-          rules={[
-            {
-              type: 'number',
-              min: 0,
-              max: 99,
-            },
-          ]}
-        >
-          <InputNumber />
-        </Form.Item>
-        <Form.Item name={['user', 'website']} label="Website">
-          <Input />
-        </Form.Item>
-        <Form.Item name={['user', 'introduction']} label="Introduction">
-          <Input.TextArea />
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+      <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onSubmit={handleFormSubmit}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]} onChange={handleInputChange}
+        value={userFormData.username}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your email!',
+          },
+        ]} onChange={handleInputChange}
+        value={userFormData.email}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}onChange={handleInputChange}
+        value={userFormData.password}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button onSubmit={handleFormSubmit} type="primary" htmlType="submit">Submit</Button>
+      </Form.Item>
+    </Form>
     </div>
   );
 };
 
-export default SignUpForm;
-
-
-
-
-  // // set initial form state
-  // const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  // // set state for form validation
-  // const [validated] = useState(false);
-  // // set state for alert
-  // const [showAlert, setShowAlert] = useState(false);
-
-  // const [addUser] = useMutation(ADD_USER);
-
-  // const handleInputChange = (event) => {
-  //   const { name, value } = event.target;
-  //   setUserFormData({ ...userFormData, [name]: value });
-  // };
-
-  // const handleFormSubmit = async (event) => {
-  //   event.preventDefault();
-
-  //   // use try/catch instead of promises to handle errors
-  //   try {
-  //     // execute addUser mutation and pass in variable data from form
-  //     const { data } = await addUser({
-  //       variables: { ...userFormData }
-  //     });
-
-  //     Auth.login(data.addUser.token)
-
-  //   } catch (e) {
-  //     console.error(e);
-  //     setShowAlert(true);
-  //   }
-
-  //   setUserFormData({
-  //     username: '',
-  //     email: '',
-  //     password: '',
-  //   });
-  // };
+export default SignupForm;
