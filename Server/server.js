30255f0df46ebdc4792daa894ceb7604e17240de
Elziekio <anthony.deamer@hotmail.com>
2021-11-13 12:52:43 +1000

    update files

--- Server/server.js
+++ Server/server.js
@@ -13,8 +13,20 @@ const PORT = process.env.PORT || 3001;
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware })
   const app = express();
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
   await server.start();
   server.applyMiddleware({ app, path: '/graphql' });
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
 
   db.once('open', () => {
     app.listen(PORT, () => {
@@ -25,16 +37,3 @@ async function startApolloServer(typeDefs, resolvers) {
 }
 
 startApolloServer(typeDefs, resolvers);
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
