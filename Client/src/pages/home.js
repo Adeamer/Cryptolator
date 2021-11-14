55868396279c2d61521752cf602d27edc52422b1
Elziekio <anthony.deamer@hotmail.com>
2021-11-14 18:48:22 +1000

    added styling to home page

--- Client/src/pages/home.js
+++ Client/src/pages/home.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from 'react';
 import SignupForm from '../components/SignupForm';
 import LoginForm from '../components/LoginForm';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch, Outlet } from "react-router-dom";
+import '../Styles/Home.css';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+import logo from '../assets/images/logo.jpg';
+import logo2 from '../assets/images/logo2.jpg';
+
 
 const client = new ApolloClient({
     uri: '/graphql',
@@ -13,25 +19,24 @@ const Home = () => {
 
 
     return (
-        <div>
-            <header>Crypulator
-                <p>Brief explination of app</p>
+        <div className="body">
+            <header>
+                <h1 className="title">Crypulator</h1>
+                <p className="explination">Tired of being stung at tax time? Plan ahead with Crypulator and get the most out of you crypto investments!</p>
             </header>
+            <div className="btn-container">
+                <p className="signup-text">First time? Sign up here</p>
                 <Link to="/signup">
-                    <button>Sign Up</button>
+                <Button type="default" className="signup-btn">Sign up</Button>
                 </Link>
+                <p className="login-text">Login here</p>
                 <Link to="/login">
-                    <button>login</button>
+                    <Button type="default" className="login-btn">login</Button>
                 </Link>
-                <Switch>
-                    <Route exact path='/signup'>
-                        <SignupForm />
-                    </Route>
-                    <Route exact path='/login'>
-                        <LoginForm />
-                    </Route>
-                </Switch>
-                {/* Image of calculator and graph */}
+                <Outlet />
+            </div>
+            <img className="logo" src={logo} alt="crypto-calculator"/>
+            <img className="logo2" src={logo2} alt="crypto"/>
         </div>
     )
 };
