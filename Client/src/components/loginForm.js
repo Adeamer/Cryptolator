b5c4824f7a79d2a8c996c2ded672f88f4833490c
Elziekio <anthony.deamer@hotmail.com>
2021-11-14 18:47:55 +1000

    added login form

--- Client/src/components/loginForm.js
+++ Client/src/components/loginForm.js
@@ -1,9 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
 import 'antd/dist/antd.css';
+
 import { Form, Input, Button, Checkbox } from 'antd';
 
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+import { Redirect } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import { useNavigate } from 'react-router-dom';
+
+
+
+
 const LoginForm = () => {
+    const navigate = useNavigate();
+    const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+
+    const [loginUser, { error }] = useMutation(LOGIN_USER);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({ ...userFormData, [name]: value });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await loginUser({
+                variables: { ...userFormData }
+            });
+
+            Auth.login(data.login.token)
+            navigate('dashboard');
+
+        } catch (err) {
+            console.error(err);
+        }
+
+        setUserFormData({
+            username: '',
+            email: '',
+            password: '',
+        });
+    };
+
     const onFinish = (values) => {
         console.log('Success:', values);
     };
@@ -15,70 +59,38 @@ const LoginForm = () => {
     return (
         <div>
             <h1>Login</h1>
-            <Form
-                name="basic"
-                labelCol={{
-                    span: 8,
-                }}
-                wrapperCol={{
-                    span: 16,
-                }}
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                    label="Name"
-                    name="name"
-                    rules={[
+            <Form name="basic" labelCol={{ span: 8, }} wrapperCol={{ span: 16, }} initialValues={{ remember: true, }}
+             onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off" onSubmit={handleFormSubmit}>
+                <Form.Item label="Email" name="email" rules={[
                         {
                             required: true,
-                            message: 'Please input your name!',
+                            message: 'Please input your email!',
                         },
-                    ]}
-                >
+                    ]} onChange={handleInputChange} value={userFormData.email}>
                     <Input />
                 </Form.Item>
 
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
+                <Form.Item label="Password" name="password" rules={[
                         {
                             required: true,
                             message: 'Please input your password!',
                         },
-                    ]}
-                >
+                    ]} onChange={handleInputChange} value={userFormData.password}>
                     <Input.Password />
                 </Form.Item>
 
-                <Form.Item
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{
+                <Form.Item name="remember" valuePropName="checked" wrapperCol={{
                         offset: 8,
                         span: 16,
-                    }}
-                >
+                    }}>
                     <Checkbox>Remember me</Checkbox>
                 </Form.Item>
 
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
+                <Form.Item wrapperCol={{ offset: 8, span: 16, }}>
+                    <Button onClick={handleFormSubmit} type="primary" htmlType="submit">Submit</Button>
                 </Form.Item>
             </Form>
-        </div>
+        </div >
     );
 };
 
