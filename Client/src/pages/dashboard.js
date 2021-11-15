227aec932c590181e4b0cfe0734ed78cd4a7c215
Elziekio <anthony.deamer@hotmail.com>
2021-11-15 13:03:57 +1000

    added css stlying to dashboard

--- Client/src/pages/dashboard.js
+++ Client/src/pages/dashboard.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import Graph from '../components/Graph';
+import "../Styles/Dashboard.css";
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
 
     return (
         <div>
             <h1>Hello username</h1>
-            <button>Logout</button>
+            <Link to="/login">
+                    <Button type="default" className="logout-btn">Logout</Button>
+            </Link>
+
             <div className='walletContainer'>
-                <h2>Wallet</h2>
-                <p>Currencies</p>
-                <p>Amount</p>
-                <h3>Total</h3>
+                <h2 className="wallet-text">Wallet</h2>
+                <p className="currency-text">Currencies</p>
+                <p className="amount-text">Amount</p>
+                <h3 className="total-text">Total</h3>
             </div>
             <div className='graphContainer'>
                
