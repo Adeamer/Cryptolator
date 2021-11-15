e25d9cf25045d9a1e076aceca0885ed7b6af0fe0
Elziekio <anthony.deamer@hotmail.com>
2021-11-15 22:00:27 +1000

    amended valifate messages

--- Client/src/components/signupForm.js
+++ Client/src/components/signupForm.js
@@ -23,10 +23,6 @@ const validateMessages = {
   required: '${label} is required!',
   types: {
     email: '${label} is not a valid email!',
-    number: '${label} is not a valid number!',
-  },
-  number: {
-    range: '${label} must be between ${min} and ${max}',
   },
 };
 
