ad3e77025bca73427109df31a65c3cca634b27b8
Elziekio <anthony.deamer@hotmail.com>
2021-11-15 22:00:57 +1000

    add props for add currency form

--- Client/src/pages/dashboard.js
+++ Client/src/pages/dashboard.js
@@ -3,26 +3,44 @@ import Graph from '../components/Graph';
 import "../Styles/Dashboard.css";
 import 'antd/dist/antd.css';
 import { Button } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, Route, Routes } from 'react-router-dom';
+import { useMutation, gql } from "@apollo/client";
+import AddCurrency from './addCurrency';
 
-const Dashboard = () => {
+
+
+const Dashboard = (props) => {
+    const navigate = useNavigate();
+
+    const currencyName = props.currencyName;
+    const soldAmount = props.soldAmount;
+    console.log(currencyName);
+    console.log(soldAmount);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        navigate('/addCurrency', { replace: true });
+    };
 
     return (
         <div>
-            <h1>Hello username</h1>
-            <Link to="/login">
+            <h1 className="dashboard-title">Hello Anthony Deamer</h1>
+            <Link to="/">
                     <Button type="default" className="logout-btn">Logout</Button>
             </Link>
 
             <div className='walletContainer'>
                 <h2 className="wallet-text">Wallet</h2>
-                <p className="currency-text">Currencies</p>
+                <p className="currency-text">Currency</p>
+                <p className="currencyName">{currencyName}</p>
                 <p className="amount-text">Amount</p>
-                <h3 className="total-text">Total</h3>
+                <p className="sold-amount">{soldAmount}</p>
+                <Button onClick={handleSubmit} type="primary" className="addCurrency-btn">Add Currency</Button>
             </div>
-            <div className='graphContainer'>
+            <div className='total-tax'>
                
-                <p>Total amount of tax</p>
+                <p className="tax-title">Total amount of tax</p>
+                
             </div>
         </div>
     )
